a7bec67388b37ea69a5a64170f70e610265afa43
Matthijs Kooijman <matthijs@stdin.nl>
2014-03-04 12:52:15 +0100

    Increase MAX_USER_FUNCTIONS
    Recent changes to the library-pinoccio repository increased the number
    of functions past 100, causing Functions: overflow errors.

--- src/bitlash-functions.c
+++ src/bitlash-functions.c
@@ -391,7 +391,7 @@ const bitlash_function function_table[] PROGMEM = {
 #endif
 
 #ifdef USER_FUNCTIONS
-#define MAX_USER_FUNCTIONS 100		// increase this if needed, but keep free() > 200 ish
+#define MAX_USER_FUNCTIONS 150		// increase this if needed, but keep free() > 200 ish
 #define USER_FUNCTION_FLAG 0x80
 
 typedef struct {
